03855373f8c35ba570aa3244b263642a0e17c817
od-8 <178620777+od-8@users.noreply.github.com>
2025-04-23 00:38:34 +0100

    feat: Began work on game tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,10 +3,19 @@ require_relative "../lib/game"
 
 describe Game do
   describe "#game_loop" do
-    # Tests will be here
+    # tests to be
   end
 
   describe "#player_turn" do
-    # Tests will be here
+    let(:new_board) { double("board") }
+    let(:player1) { double("player") }
+    let(:player2) { double("player") }
+    subject(:test_game) { described_class.new(new_board, player1, player2) }
+
+    context "returns column and invalid_moves" do
+      # Write a test that stops when valid_move?(column) and !full_row?(column)
+      # returns true and not true, when column is 1 receive message once
+      it "ret"
+    end
   end
 end
